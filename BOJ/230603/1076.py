830ab2b0956621349962273dcb89d40e8e0944a8
potato3641 <gearg1456@gmail.com>
2023-06-03 11:04:12 +0900

    '2023-06-03-commit'

---
+++ BOJ/230603/1076.py
@@ -0,0 +1,2 @@
+colors = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
+print((colors.index(input())*10+colors.index(input()))*(10**colors.index(input())))
